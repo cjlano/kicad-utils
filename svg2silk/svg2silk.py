50b38dd448b3b6dea2fcbf360085b23568169de1
CJlano <cjlano+github@free.fr>
2013-07-19 21:18:23 +0200

    [svg2silk] Add SVG and KiCad libraries as submodules

--- svg2silk/svg2silk.py
+++ svg2silk/svg2silk.py
@@ -16,8 +16,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import kicad
-import svg
+from kicad import *
+from svg import *
 import sys
 
 f = svg.Svg(sys.argv[1])
