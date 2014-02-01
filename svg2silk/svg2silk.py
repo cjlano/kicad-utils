25e41c987e875e02969b6001c460c128af70c3fd
CJlano <cjlano+github@free.fr>
2014-02-01 11:56:04 +0100

    Fix kicad import

--- svg2silk/svg2silk.py
+++ svg2silk/svg2silk.py
@@ -16,7 +16,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from kicad import *
+#from kicad import *
+import kicad
 import svg
 import sys
 import copy
