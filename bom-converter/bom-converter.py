5b7fb1eaa72e395cbce833386c53c0522922bf6c
simon gansen <simon@sineband.de>
2015-11-17 16:01:26 +0100

    Add some informative std-output

--- bom-converter/bom-converter.py
+++ bom-converter/bom-converter.py
@@ -18,6 +18,7 @@ class XmlBom(object):
     def __init__(self):
         self.title = ''
         self.components = []
+        self.filename = ''
 
     def __str__(self):
         out = ''
@@ -29,6 +30,8 @@ class XmlBom(object):
         return out
 
     def load(self, filename):
+        print('*** Parsing input file ...')
+        self.filename = filename
         xml_tree = etree.parse(filename)
         xml_root = xml_tree.getroot()
 
@@ -37,9 +40,9 @@ class XmlBom(object):
             # write component into dictionary
             component = {}
             component['designators'] = [comp.attrib['ref']] # designators key contains list
-            component['value'] = "" if comp.find('value') is None else comp.find('value').text.strip()
-            component['footprint'] = "" if comp.find('footprint') is None else comp.find('footprint').text.strip()
-            component['datasheet'] = "" if comp.find('datasheet') is None else comp.find('datasheet').text.strip()
+            component['value'] = '' if comp.find('value') is None else comp.find('value').text.strip()
+            component['footprint'] = '' if comp.find('footprint') is None else comp.find('footprint').text.strip()
+            component['datasheet'] = '' if comp.find('datasheet') is None else comp.find('datasheet').text.strip()
 
             fields = comp.find('fields');
             if fields is not None:
@@ -73,10 +76,10 @@ class XmlBom(object):
                     except KeyError:
                         if key in m and key in c:
                             match = True
-                            print('Empty field ', key, ' in ', c['designators'][0])
+                            print('Empty field', key, 'in', c['designators'][0])
                         elif not key in m and not key in c:
                             match = True
-                            print('Missing field ', key, ' in ', c['designators'][0])
+                            print('Missing field' , key, 'in', c['designators'][0])
                         else:
                             match = False
                         break
@@ -92,6 +95,7 @@ class XmlBom(object):
                 merged_components.append(c)
 
         # replace global list
+        print('Found', len(merged_components),'unique component types in', self.filename)
         self.components = merged_components
 
     def count(self):
@@ -117,6 +121,7 @@ class HtmlBom(object):
         self.xml_bom = xml_bom
 
     def get_html_string(self):
+        print('*** Generating HTML ...')
 
         # header, style sheets and table start
         html = ''
@@ -208,6 +213,8 @@ class CsvBom(object):
         self.xml_bom = xml_bom
 
     def get_csv_string(self):
+        print('*** Generating CSV for', len(self.xml_bom.components), 'unique component types')
+        row_count = 0
         seperator = ', '
         csv = 'QTY,\tSupplier Part Number,\tManufacturer Part Number\n'
         for c in self.xml_bom.components:
@@ -218,7 +225,9 @@ class CsvBom(object):
 
             row = seperator.join(row_sequence)
             csv += row + '\n'
+            row_count += 1
 
+        print('Added', str(row_count), 'rows')
         return csv
 
 if __name__ == "__main__":
@@ -241,19 +250,19 @@ if __name__ == "__main__":
     xml_bom.count()
     xml_bom.sort()
 
-    # create html string
-    html_bom = HtmlBom(xml_bom)
-    html = html_bom.get_html_string()
-
     # if required, create a csv file
     if args.csv_output:
         csv_bom = CsvBom(xml_bom)
         csv = csv_bom.get_csv_string()
         with open(args.csv_output, 'w') as csv_file:
             csv_file.write(csv)
-            print('CSV file', csv_file.name, 'written')
+            print('*** CSV file', csv_file.name, 'written')
+
+    # create html string
+    html_bom = HtmlBom(xml_bom)
+    html = html_bom.get_html_string()
 
     # write html to file
     with open(args.htmlfile, 'w') as html_file:
         html_file.write(html)
-        print('HTML file ' + html_file.name + ' written')
+        print('*** HTML file ' + html_file.name + ' written')
